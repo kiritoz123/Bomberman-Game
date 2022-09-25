591cff931c81dea2cc416f8a9a8244714863895b
kiritoz123 <hn61078@gmail.com>
2022-09-25 15:52:56 +0700

    1

--- src/oop/bomberman/level/FileLevel.java
+++ src/oop/bomberman/level/FileLevel.java
@@ -1,2 +1,16 @@
-package oop.bomberman.level;public class FileLevel {
+package oop.bomberman.level;
+
+public class FileLevel {
+    private int level;
+
+    public int getLevel() {
+        return level;
+    }
+
+    public void setLevel(int level) {
+        this.level = level;
+    }
+
+    public FileLevel() {
+    }
 }
