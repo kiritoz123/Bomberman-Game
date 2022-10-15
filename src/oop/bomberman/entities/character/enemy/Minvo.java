bd9352d1fb676860e785ed1767822edc091cb331
kiritoz123 <hn61078@gmail.com>
2022-10-16 00:01:38 +0700

    merge

---
+++ src/oop/bomberman/entities/character/enemy/Minvo.java
@@ -0,0 +1,14 @@
+package oop.bomberman.entities.character.enemy;
+
+import javafx.scene.image.Image;
+
+public class Minvo extends Enemy{
+    public Minvo(int xUnit, int yUnit, Image img) {
+        super(xUnit, yUnit, img);
+    }
+
+    @Override
+    public void update() {
+
+    }
+}
